ed78337df8594775e16df2264cfbffde0dab878d
Orlando Mota Pires <orlandomota2002@gmail.com>
2022-07-07 16:21:22 -0300

    Test

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+app.use('/pedidos', rotaPedidos); 
 
 module.exports = app;
