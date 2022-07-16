a37d101853f3f6cb4d0b1af78190300b2b48a05e
Orlando Mota Pires <orlandomota2002@gmail.com>
2022-07-16 19:33:08 -0300

    Some changes

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,49 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Retorna todos os produtos
-router.get('/', (req, res, next) =>{
-  res.status(200).send({
-    mensagem: 'Retorna os produtos'
-  })
+router.get("/", (req, res, next) => {
+	res.status(200).send({
+		mensagem: "Retorna os produtos",
+	});
 });
 
 //Insere um produto
-router.post('/', (req, res, next)=>{
+router.post("/", (req, res, next) => {
+	const produto = {
+		//Objeto produto tem nome e preco
+		nome: req.body.nome,
+		preco: req.body.preco,
+	};	
 
-  const produto = { //Objeto produto tem nome e preco
-    nome: req.body.nome,
-    preco: req.body.preco
-  };
-
-  res.status(201).send({
-    mensagem: 'Insere um produto',
-    produtoCriado: produto
-
-  })
-
+	res.status(201).send({
+		mensagem: "Insere um produto",
+		produtoCriado: produto,
+	});
 });
 
 //Retorna os dados de um produto
-router.get('/:id_produto', (req, res, next) =>{
-  const id = req.params.id_produto;
-  res.status(200).send({
-    mensagem: 'Produto único encontrado'
-  });
-})
+router.get("/:id_produto", (req, res, next) => {
+	const id = req.params.id_produto;
+	res.status(200).send({
+		mensagem: "Produto único encontrado",
+	});
+});
 
 //Altera um produto
-router.patch('/', (req, res, next) =>{
-  res.status(201).send({
-    mensagem: 'Produto alterado'
-  })
-})
+router.patch("/", (req, res, next) => {
+	res.status(201).send({
+		mensagem: "Produto alterado",
+	});
+});
 
 //Exclui um produto
-router.delete('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Produto excluído'
-  })
-})
+router.delete("/", (req, res, next) => {
+	res.status(201).send({
+		mensagem: "Produto excluído",
+	});
+});
 
 module.exports = router;
